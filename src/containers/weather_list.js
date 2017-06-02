a0ad1074e59cc4da553e78d2e738e742ae0d70e4
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-02 23:20:44 +0300

    Sent weather object with reducer index to container component WetherList

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component{
+class WeatherList extends Component{
     render(){
+        console.log(this.props.weather);
         return(
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
                 </tbody>
             </table>
         );
@@ -18,3 +28,14 @@ export default class WeatherList extends Component{
 
 }
 
+//ES5
+// function mapStateToProps(state){
+//     return { weather : state.weather }
+// }
+// в ES6 
+function mapStateToProps({weather}){//const weather = state.weather ES5
+    return { weather } //weather : weather ES5
+    // {weather} === {weather:weather}
+}
+
+export default connect(mapStateToProps)(WeatherList); 
