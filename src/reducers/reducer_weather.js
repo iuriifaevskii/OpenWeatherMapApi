cedf693718d456956ea5b16ee61982f2dbd55b8b
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-02 23:04:30 +0300

    Create city table component

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -7,7 +7,7 @@ export default function(state = [], action){
         case FETCH_WEATHER:
         console.log([action.payload.data, ...state]);
         return [action.payload.data, ...state]; //[city, city, city] NOT [city, [city,city]]
-        // приблизно це саме, бо ми повертаємо state.push( action.payload );
+        // Не можна маніпулювати існуючим STATE! state.push( action.payload );
         // state.concat([action.state.data]) - правильніше! але в ES6 робимо ...
         // але так не робимо бо можна тільки робити setState! 
         // повертаємо новий масив з +1 містом!
