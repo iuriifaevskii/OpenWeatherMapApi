55e2e3c6982cd219102e2ebffdaab10323e1c55e
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-02 19:24:26 +0300

    install axios and create get request in redux actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'd6c84c2bbd5e1e260db4404caf691e8c';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},ua`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
